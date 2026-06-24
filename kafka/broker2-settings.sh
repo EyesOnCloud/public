dd7c9ce4944a052157166e9502c5c764cce3b4f2
EyesOnCloud <naushad.p.tech@gmail.com>
2026-06-24 13:02:58 +0530

    Update broker2-settings.sh

--- kafka/broker2-settings.sh
+++ kafka/broker2-settings.sh
@@ -6,6 +6,6 @@ sed -i 's|^listeners=.*|listeners=PLAINTEXT://:9094,CONTROLLER://:9095|' "$CONFI
 
 sed -i 's|^advertised.listeners=.*|advertised.listeners=PLAINTEXT://localhost:9094|' "$CONFIG_FILE"
 
-sed -i 's|^controller.quorum.voters=.*|controller.quorum.voters=1@localhost:9093,2@localhost:9095,3@localhost:9097|' "$CONFIG_FILE"
+echo 'controller.quorum.voters=1@localhost:9093,2@localhost:9095,3@localhost:9097' >> "$CONFIG_FILE"
 
 sed -i 's|^log.dirs=.*|log.dirs=/opt/kafka/data-broker2|' "$CONFIG_FILE"
