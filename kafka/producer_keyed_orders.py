db678dc06f08d8d43c23d58f6676420ccd22637f
EyesOnCloud <naushad.p.tech@gmail.com>
2026-07-09 18:46:46 +0530

    Update producer_keyed_orders.py

--- kafka/producer_keyed_orders.py
+++ kafka/producer_keyed_orders.py
@@ -6,7 +6,7 @@ from confluent_kafka import Producer
 
 TOPIC = 'orders-group-demo'
 MESSAGE_COUNT = 100
-BOOTSTRAP_SERVERS = '192.168.100.21:9092,192.168.100.22:9092,192.168.100.23:9092'
+BOOTSTRAP_SERVERS = '192.168.100.11:9092,192.168.100.12:9092,192.168.100.13:9092'
 
 def delivery_callback(err, msg):
     if err:
